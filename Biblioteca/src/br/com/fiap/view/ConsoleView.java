3cacea7ece22863a3c68a3a51cdfb9ff6fd5916a
Thiago <thiago.sabatini.10@gmail.com>
2020-04-26 08:27:31 -0300

    ccommit conseoleView2

--- Biblioteca/src/br/com/fiap/view/ConsoleView.java
+++ Biblioteca/src/br/com/fiap/view/ConsoleView.java
@@ -10,9 +10,18 @@ import javax.persistence.Persistence;
 
 import br.com.fiap.dao.ClienteDAO;
 import br.com.fiap.dao.Impl.ClienteDAOImpl;
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.entity.Editora;
+import br.com.fiap.entity.Forma;
+import br.com.fiap.entity.Genero;
+import br.com.fiap.entity.Idioma;
 import br.com.fiap.entity.Livro;
 import br.com.fiap.entity.Locacao;
+import br.com.fiap.entity.Pagamento;
+import br.com.fiap.entity.Status;
 import br.com.fiap.entity.TipoLocacao;
+import br.com.fiap.exception.CommitException;
+import br.com.fiap.entity.Idioma;
 
 
 public class ConsoleView {
@@ -26,8 +35,31 @@ public class ConsoleView {
 		
 		Locacao locacao = new Locacao(Calendar.getInstance(), Calendar.getInstance() , 50.0f, TipoLocacao.SEMANAL, 2);
 		
-		List<Livro> livros = new ArrayList();
+		List<Livro> livros = new ArrayList<Livro>();
 		
-		livros.add(new Livro());
+		livros.add(new Livro("A Origem das Especies", Genero.LITERATURA_CIENTIFICA, "Charles Darwin", Idioma.PORTUGUES,
+				new Editora("Martin Claret")));
+		
+		livros.add(new Livro("O Hobbit", Genero.FANTASIA, "J.R.R Tolkien", Idioma.PORTUGUES,
+				new Editora("Harper Collins")));
+		
+		locacao.setLivros(livros);
+		
+		locacao.setPagamento(new Pagamento(Status.PENDENTE, Forma.CREDITO, 50.0f));
+		
+		Cliente cliente = new Cliente("Thiago", "234.345.345-56", "98345-9432");
+		
+		cliente.adcLocacao(locacao);
+		
+		dao.cadastrar(cliente);
+		try {
+			dao.commit();
+			System.out.println("Sucesso!");
+		} catch (CommitException e) {
+			System.out.println("Erro..");
+		}
+		
+		em.close();
+		fabrica.close();
 	}
 }
